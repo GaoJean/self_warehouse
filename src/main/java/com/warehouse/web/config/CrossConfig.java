87255bd7100c5a8431bad296fcfd82e9b437f2da
gaojian <gaojian@doctorwork.com>
2021-12-05 18:42:30 +0800

    处理跨域

---
+++ src/main/java/com/warehouse/web/config/CrossConfig.java
@@ -0,0 +1,22 @@
+package com.warehouse.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description:
+ * @Author: gaojian@doctorwork.com
+ * @Date: 2021/12/05 18:23
+ */
+@Configuration
+public class CrossConfig implements WebMvcConfigurer {
+
+    public static final int MAX_AGE = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*").allowCredentials(true).maxAge(MAX_AGE)
+            .allowedHeaders("*");
+    }
+}
