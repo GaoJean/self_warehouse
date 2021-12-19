d4311a01c4de4cb21e1fa90981775e0c46e02ec8
gaojian <gaojian@doctorwork.com>
2021-12-19 21:47:52 +0800

    时间工具

--- src/main/java/com/warehouse/common/error/ApiError.java
+++ src/main/java/com/warehouse/common/error/ApiError.java
@@ -6,8 +6,30 @@ package com.warehouse.common.error;
  * @Date: 2021/06/03 18:29
  */
 public enum ApiError implements ErrorCode {
-    SERVER_ERROR(1, "服务异常"), BASE_NOT_FOUND(2, "资源不存在"), BASE_UNAUTHORIZED(3, "未授权"), BASE_FORBIDDEN(4,
-        "拒绝访问"), BASE_BAD_PARAMS(5, "请求参数错误"), MANAGEMENT_TYPE_ERROR(6, "操作类型错误");
+    /**
+     * 服务异常
+     */
+    SERVER_ERROR(1, "服务异常"),
+    /**
+     * 资源不存在
+     */
+    BASE_NOT_FOUND(2, "资源不存在"),
+    /**
+     * 时间格式错误
+     */
+    TIME_ERROR(3, "时间格式错误"),
+    /**
+     * 拒绝访问
+     */
+    BASE_FORBIDDEN(4, "拒绝访问"),
+    /**
+     * 请求参数错误
+     */
+    BASE_BAD_PARAMS(5, "请求参数错误"),
+    /**
+     * 操作类型错误
+     */
+    MANAGEMENT_TYPE_ERROR(6, "操作类型错误");
 
     private int code;
 
@@ -18,10 +40,12 @@ public enum ApiError implements ErrorCode {
         this.message = message;
     }
 
+    @Override
     public int getCode() {
         return this.code;
     }
 
+    @Override
     public String getMessage() {
         return this.message;
     }
--- src/main/java/com/warehouse/common/util/DateUtil.java
+++ src/main/java/com/warehouse/common/util/DateUtil.java
@@ -1,5 +1,15 @@
 package com.warehouse.common.util;
 
+import static com.warehouse.common.error.ApiError.TIME_ERROR;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.warehouse.common.exception.HouseException;
+
 /**
  * @Description:
  * @Author: gaojian@doctorwork.com
@@ -10,4 +20,24 @@ public class DateUtil {
      * 格式化通配符: yyyy-MM-dd HH:mm:ss
      */
     public static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 字符串格式转换为date
+     *
+     * @param str
+     * @return
+     * @throws HouseException
+     */
+    public static Date formatToTime(String str) throws HouseException {
+        try {
+            if (StringUtils.isBlank(str)) {
+                throw new HouseException(TIME_ERROR);
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME);
+            return sdf.parse(str);
+        } catch (ParseException parseException) {
+            throw new HouseException(TIME_ERROR);
+        }
+    }
+
 }
